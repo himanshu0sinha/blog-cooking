9a9d5b8d5e05a65c8807ddc6c0546453c991739f
himanshu0sinha <500091727@stu.upes.ac.in>
2024-03-18 07:21:46 +0530

    adding code

--- app.js
+++ app.js
@@ -1,32 +1,88 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const fileUpload = require('express-fileupload');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const flash = require('connect-flash');
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    // In order to use PUT HTTP verb to edit item
+    methodOverride = require('method-override'),
+    // Mitigate XSS using sanitizer
+    sanitizer = require('sanitizer'),
+    app = express(),
+    port = 8000
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-require('dotenv').config();
-
-app.use(express.urlencoded( { extended: true } ));
-app.use(express.static('public'));
-app.use(expressLayouts);
-
-app.use(cookieParser('CookingBlogSecure'));
-app.use(session({
-  secret: 'CookingBlogSecretSession',
-  saveUninitialized: true,
-  resave: true
+app.use(bodyParser.urlencoded({
+    extended: false
 }));
-app.use(flash());
-app.use(fileUpload());
+// https: //github.com/expressjs/method-override#custom-logic
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method;
+        delete req.body._method;
+        return method
+    }
+}));
 
-app.set('layout', './layouts/main');
-app.set('view engine', 'ejs');
 
-const routes = require('./server/routes/recipeRoutes.js')
-app.use('/', routes);
+let todolist = [];
 
-app.listen(port, ()=> console.log(`Listening to port ${port}`));
+/* The to do list and the form are displayed */
+app.get('/todo', function (req, res) {
+        res.render('todo.ejs', {
+            todolist,
+            clickHandler: "func1();"
+        });
+    })
+
+    /* Adding an item to the to do list */
+    .post('/todo/add/', function (req, res) {
+        // Escapes HTML special characters in attribute values as HTML entities
+        let newTodo = sanitizer.escape(req.body.newtodo);
+        if (req.body.newtodo != '') {
+            todolist.push(newTodo);
+        }
+        res.redirect('/todo');
+    })
+
+    /* Deletes an item from the to do list */
+    .get('/todo/delete/:id', function (req, res) {
+        if (req.params.id != '') {
+            todolist.splice(req.params.id, 1);
+        }
+        res.redirect('/todo');
+    })
+
+    // Get a single todo item and render edit page
+    .get('/todo/:id', function (req, res) {
+        let todoIdx = req.params.id;
+        let todo = todolist[todoIdx];
+
+        if (todo) {
+            res.render('edititem.ejs', {
+                todoIdx,
+                todo,
+                clickHandler: "func1();"
+            });
+        } else {
+            res.redirect('/todo');
+        }
+    })
+
+    // Edit item in the todo list 
+    .put('/todo/edit/:id', function (req, res) {
+        let todoIdx = req.params.id;
+        // Escapes HTML special characters in attribute values as HTML entities
+        let editTodo = sanitizer.escape(req.body.editTodo);
+        if (todoIdx != '' && editTodo != '') {
+            todolist[todoIdx] = editTodo;
+        }
+        res.redirect('/todo');
+    })
+    /* Redirects to the to do list if the page requested is not found */
+    .use(function (req, res, next) {
+        res.redirect('/todo');
+    })
+
+    .listen(port, function () {
+        // Logging to console
+        console.log(`Todolist running on http://0.0.0.0:${port}`)
+    });
+// Export app
+module.exports = app;
